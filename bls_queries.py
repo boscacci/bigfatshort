17e7821dbee4cf53360738e08631893eb3bbaf06
cinemarob1 <cinemarob1@gmail.com>
2018-11-10 16:59:07 -0500

    Making sure it's updated

--- bls_queries.py
+++ bls_queries.py
@@ -4,9 +4,13 @@ BLS_API_KEY = '5d111c391937452b82d6878bbb76783c'
 
 # Show avg expenditures in urban vs rural areas
 
-s1 = 'CXURNTDWELLLB0806M' # avg spending on rent: urban renters
-s2 = 'CXURNTDWELLLB0809M' # avg spending on rent: rural renters
-s3 = 'CXUTRANSLB0806M' # avg spending on transpo: urban renters
-s4 = 'CXUTRANSLB0809M' # avg spending on transpo: rural renters
+s1 = 'CXURNTDWELLLB0806M' # avg ann. spending on rent: urban renters
+s2 = 'CXURNTDWELLLB0809M' # avg ann. spending on rent: rural renters
+s3 = 'CXUTRANSLB0806M' # avg ann. spending on transpo: urban renters
+s4 = 'CXUTRANSLB0809M' # avg ann. spending on transpo: rural renters
+
+# A year has several geographic areas
+# A geo area has many expenditures
+# Expenditure values belong to a year
 
 series = BLS.get_series([s1, s2, s3, s4], 1980, 2017, BLS_API_KEY)
