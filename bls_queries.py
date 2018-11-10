8bc54541ff9a216486fb81a75edebe2ce6081260
cinemarob1 <cinemarob1@gmail.com>
2018-11-10 12:09:26 -0500

    More links to resources

--- bls_queries.py
+++ bls_queries.py
@@ -2,7 +2,11 @@ from subscripts import bls_api_query_app as BLS
 
 BLS_API_KEY = '5d111c391937452b82d6878bbb76783c'
 
-# Show avg expenditure on rent in urban vs rural areas
-s1 = 'CXURNTDWELLLB0806M' # urban
-s2 = 'CXURNTDWELLLB0809M' # rural
-series = BLS.get_series([s1, s2], 1980, 2017, BLS_API_KEY)
+# Show avg expenditures in urban vs rural areas
+
+s1 = 'CXURNTDWELLLB0806M' # avg spending on rent: urban renters
+s2 = 'CXURNTDWELLLB0809M' # avg spending on rent: rural renters
+s3 = 'CXUTRANSLB0806M' # avg spending on transpo: urban renters
+s4 = 'CXUTRANSLB0809M' # avg spending on transpo: rural renters
+
+series = BLS.get_series([s1, s2, s3, s4], 1980, 2017, BLS_API_KEY)
