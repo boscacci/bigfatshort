dca56bd2d798e5530c02c4b8a4f739b0be7adf59
cinemarob1 <cinemarob1@gmail.com>
2018-11-10 11:58:08 -0500

    Gathering external resources

--- bls_queries.py
+++ bls_queries.py
@@ -2,4 +2,7 @@ from subscripts import bls_api_query_app as BLS
 
 BLS_API_KEY = '5d111c391937452b82d6878bbb76783c'
 
-series = BLS.get_series(['CUUR0000SA0L1E'], 2000, 2015, BLS_API_KEY)
+# Show avg expenditure on rent in urban vs rural areas
+s1 = 'CXURNTDWELLLB0806M' # urban
+s2 = 'CXURNTDWELLLB0809M' # rural
+series = BLS.get_series([s1, s2], 1980, 2017, BLS_API_KEY)
