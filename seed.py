ce40b06dd69c1fc96fa748b4f1e265a1be56c324
cinemarob1 <cinemarob1@gmail.com>
2018-11-11 18:25:59 -0500

    Got json data back in

---
+++ seed.py
@@ -0,0 +1,36 @@
+import json, pdb
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+
+app = Flask(__name__)
+
+with open('BLM_data.json') as BLM_data:
+    data = json.load(BLM_data)
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///BLS_data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Year(db.Model):
+    __tablename__ = 'years'
+    id = db.Column(Integer, primary_key = True)
+    year = db.Column(Integer)
+    demographics = db.relationship('Demographic', back_populates='years')
+
+class Demographic(db.Model):
+    __tablename__ = 'demographics'
+    id = db.Column(Integer, primary_key = True)
+    year_id = db.Column(Integer, ForeignKey('years.id'))
+    edu_level = db.Column(Text)
+    expenses = db.relationship('Expense', back_populates='demographic')
+    years = db.relationship('Year', back_populates='demographics')
+
+class Expense(db.Model):
+    __tablename__ = 'expenses'
+    id = db.Column(Integer, primary_key = True)
+    category = db.Column(Text)
+    demographic_id = db.Column(Integer, ForeignKey('demographics.id'))
+    demographic = db.relationship('Demographic', back_populates='expenses')
